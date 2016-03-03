16b8c50d0b024f56da29f92c99157fdb6649213e
Valerio Masciotta <sviluppo@valeriomasciotta.it>
2016-03-03 10:41:57 +0000

    adding code

---
+++ app/code/community/Vmasciotta/QuickProductView/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+require_once Mage::getModuleDir('controllers', 'Mage_Catalog').DS.'ProductController.php';
+
+/**
+ * Class Vmasciotta_QuickProductView_IndexController
+ */
+class Vmasciotta_QuickProductView_IndexController extends Mage_Catalog_ProductController
+{
+
+}
